f15a59c7527f16d1a79a5b3d18d6e811daad5616
tobam <tobamohaqqeq84@gmail.com>
2025-12-11 00:54:00 +0000

    2nd

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
-#include <fstream>>
+#include <fstream>
+#include <string>
+#include <map>
+ using namespace std;
+
+int main(){
+    map<string, int> traffic;
+
+    ifstream infile("210-final-1=FA25.txt");
+    if (!infile){
+        cout << "Error opening file." << endl;
+        return 1;
+    }
+    string origin , destination;
+    while(infile >> origin >> destination){
+        traffic[origin] ++;
+        traffic[destination] ++;
+    }
+    return 0;
+}
