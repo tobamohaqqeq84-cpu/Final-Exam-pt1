43b19db4c9660e3735fb4c8965ad5dbd7cc01092
tobam <tobamohaqqeq84@gmail.com>
2025-12-11 01:03:22 +0000

    Milestone 1 Compeleted :)

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@
 int main(){
     map<string, int> traffic;
 
-    ifstream infile("210-final-1=FA25.txt");
+    ifstream infile("210-final-1-FA25.txt");
     if (!infile){
         cout << "Error opening file." << endl;
         return 1;
     }
     string origin , destination;
+    
     while(infile >> origin >> destination){
         traffic[origin] ++;
         traffic[destination] ++;
     }
+    cout << "All airports and their traffic counts:" << endl;
+    for (auto it = traffic.begin(); it != traffic.end();++it){
+        cout << it->first << " " << it->second << endl;
+    }
+    cout << endl;
     return 0;
 }
