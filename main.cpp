f19ae4f5659e8d3314cf6333dc0a35e70edabbe5
tobam <tobamohaqqeq84@gmail.com>
2025-12-11 02:03:13 +0000

    Milestone 3 compeleted

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 void printAirportsInRange(const map<string, int>& traffic, int low, int high){
     cout << "Airports with traffic in range [" << low << ", " << high << "]:" << endl;
     for (auto it = traffic.begin(); it != traffic.end();++it){
-        if(it ->second >= low && it -> second <=high){
+        if(it ->second >= low && it-> second <= high){
             cout << it ->first << " " << it->second << endl; 
         }
     }
@@ -18,7 +18,7 @@ void printAirportsInRange(const map<string, int>& traffic, int low, int high){
 
 int main(){
 
-    
+    //Milestone 1.
     map<string, int> traffic;
 
     ifstream infile("210-final-1-FA25.txt");
@@ -39,16 +39,17 @@ int main(){
         cout << it->first << " " << it->second << endl;
     }
     cout << endl;
-
+    
+    //Milestone 2.
     int maxCounts = 0;
 
     for (auto it = traffic.begin(); it != traffic.end(); ++it){
-        if (it -> second > maxCounts){
-             maxCounts = it -> second;
+        if (it ->second > maxCounts){
+             maxCounts = it->second;
         }
     }
 
-    cout << "Busiest airports with traffic count " << maxCounts << ":" <<endl;
+    cout << "Busiest airport(s) with traffic count " << maxCounts << ":" <<endl;
 
     for (auto it = traffic.begin(); it != traffic.end(); ++it){
         if (it -> second == maxCounts){
@@ -56,6 +57,8 @@ int main(){
         }
     }
     cout << endl;
+
+    //Milestone 3.
     printAirportsInRange(traffic, 5, 8);
     printAirportsInRange(traffic, 9, 12);
 
